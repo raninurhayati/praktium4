c0a6b6d66679762e91974273b3d72d28327101e3
raninurhayati <Raniinur251@mhs.pelitabangsa.ac.id>
2018-11-05 10:51:31 -1200

    first commit

---
+++ latihan1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n=0;
+    int max=0;
+
+    do{
+     cout<<"masukan bilangan (masukan 0 untuk berhenti) : ";
+     cin>> n;
+
+     if (n> max)
+        max= n;
+
+    } while (n!= 0);
+    cout<< "bilangan terbesar adalah" << max;
+
+
+}
